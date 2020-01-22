a4baa7c077eeed05c8833a7a845308c55a3e0596
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-22 13:29:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,15 +13,17 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-
+numbers = {:id => 4}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-end
+ implement {:key=> value } 
+ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+
 end
 
 def update_counting_hash(hash, key)
